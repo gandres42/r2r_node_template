afb9e6248e5b2ff879325718b1becb7c0995d088
gandres42 <gandres2416@gmail.com>
2025-08-07 16:19:26 -0700

    basic pub/sub example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
-fn main() {
-    
+use futures::{executor::LocalPool, future, stream::StreamExt, task::LocalSpawnExt};
+use r2r::QosProfile;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let ctx = r2r::Context::create()?;
+    let mut node = r2r::Node::create(ctx, "node", "")?;
+    let subscriber = node.subscribe::<r2r::std_msgs::msg::String>("/topic", QosProfile::default())?;
+    let publisher = node.create_publisher::<r2r::std_msgs::msg::String>("/topic", QosProfile::default())?;
+    let mut timer = node.create_wall_timer(std::time::Duration::from_millis(1000))?;
+
+    // Set up a simple task executor.
+    let mut pool = LocalPool::new();
+    let spawner = pool.spawner();
+
+    // Run the subscriber in one task, printing the messages
+    spawner.spawn_local(async move {
+        subscriber
+            .for_each(|msg| {
+                println!("got new msg: {}", msg.data);
+                future::ready(())
+            })
+            .await
+    })?;
+
+    // Run the publisher in another task
+    spawner.spawn_local(async move {
+        let mut counter = 0;
+        loop {
+            let _elapsed = timer.tick().await.unwrap();
+            let msg = r2r::std_msgs::msg::String {
+                data: format!("Hello, world! ({})", counter),
+            };
+            publisher.publish(&msg).unwrap();
+            counter += 1;
+        }
+    })?;
+
+    // Main loop spins ros.
+    loop {
+        node.spin_once(std::time::Duration::from_millis(100));
+        pool.run_until_stalled();
+    }
 }
